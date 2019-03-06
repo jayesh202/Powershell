3fb1269455e0058a4cf852f2a17221108c92ac52
jayesh202 <48145494+jayesh202@users.noreply.github.com>
2019-03-06 18:21:32 +0530

    Add files via upload

---
+++ Task1.ps1
@@ -0,0 +1,17 @@
+﻿Get-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name DontPrettyPath -Value 1
+
+mkdir c:\Demo
+
+cd C:\Demo
+
+New-Item -Name Test.txt -Type File
+
+Add-Content C:\demo\Test.txt -Value "This is my IAC Task Number 1"
+
+$env:TEMP
+
+Output for this command is "C:\Users\ADMINI~1\AppData\Local\Temp"
+
+
