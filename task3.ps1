2d85789707df452514fb4af34e5eddba7fb751d7
jayesh202 <48145494+jayesh202@users.noreply.github.com>
2019-03-22 19:44:22 +0530

    Add files via upload

---
+++ task3.ps1
@@ -0,0 +1,14 @@
+﻿Get-ChildItem "c:\" -Directory  -Recurse | Where-Object {$_.Name -match "^[0-9]{2}$"}
+
+Get-Content -Path C:\Windows\WindowsUpdate.log | Select-String "(Agent*)" | Out-GridView
+
+get-help Select-String -Examples
+
+"^[0-9]{3}[.][0-9]{3}[.][0-9]{3}[.][0-9]{3}$]" - IPv4
+
+
+
+("[S][-][1][-][5][-][2][1][-][0-9]{10}[-][0-9]{10}[-][0-9]{10}[-][0-9]{4}") - ADUser SID
+  
+  [char](65)
+  [char](66)
